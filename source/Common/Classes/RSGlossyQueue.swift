c02e4239066b07c2dec5f177acf6d5ad6f022c5c
jdkizer9 <jdkizer9@users.noreply.github.com>
2018-04-24 10:39:55 -0400

    Added a function to fetch the entire queue

--- source/Common/Classes/RSGlossyQueue.swift
+++ source/Common/Classes/RSGlossyQueue.swift
@@ -39,7 +39,6 @@ open class RSGlossyQueue<T: Glossy>: SecureQueue {
         
     }
     
-    
     private func elementForPair(elementId: String, elementData: NSData) throws -> RSGlossyQueueElement? {
         guard let elementJSON = try JSONSerialization.jsonObject(with: elementData as Data, options: []) as? JSON else {
             return nil
@@ -50,6 +49,20 @@ open class RSGlossyQueue<T: Glossy>: SecureQueue {
         }
         
         return RSGlossyQueueElement(elementId: elementId, element: element)
+    }
+    
+    open func getGlossyElements() throws -> [RSGlossyQueueElement] {
+        
+        let elementPairs = try self.getElements()
+        
+        return try elementPairs.compactMap { pair in
+            guard let elementData = pair.1 as? NSData else {
+                return nil
+            }
+            
+            return try self.elementForPair(elementId: pair.0, elementData: elementData)
+        }
+        
     }
     
     open func getFirstGlossyElement() throws -> RSGlossyQueueElement? {
